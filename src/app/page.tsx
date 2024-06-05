65d1760b60e298ba10a3d0ad8102f5617ab5369b
Allyson Soares <allysontsoares@icloud.com>
2024-06-05 00:35:12 -0400

    Show case de componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,118 @@
-import Image from "next/image";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { DialogContent, DialogFooter, DialogHeader, DialogOverlay, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Fieldset, Input, Label, Legend, Field} from "@/components/ui/form-primitives";
+import MultipleSelect, { MultipleSelectOption } from "@/components/ui/multiple-select";
+import Select, { SelectOption } from "@/components/ui/select";
+
+import { useState } from "react";
+
+
+
+
+const people: SelectOption[] = [
+  { value: '1', label: "Wade Cooper" },
+  { value: '2', label: "Arlene Mccoy" },
+  { value: '3', label: "Devon Webb" },
+  { value: '4', label: "Tom Cook" },
+  { value: '5', label: "Tanya Fox" },
+  { value: '6', label: "Hellen Schmidt" },
+];
+
+const roles: MultipleSelectOption[] = [
+  { value: '1', label: "Marketing" },
+  { value: '2', label: "Front desk" },
+  { value: '3', label: "Support" },
+];
 
 export default function Home() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  function open() {
+    setIsOpen(true);
+  }
+
+  function close() {
+    setIsOpen(false);
+  }
+
+
+  const [form, setForm] = useState<{ person_id: number | null; role_ids: number[] }>({
+    person_id: null,
+    role_ids: [],
+  });
+
+  const handlePersonChange = (value: number | null) => {
+    setForm((prevForm) => ({
+      ...prevForm,
+      person_id: value,
+    }));
+  };
+
+  const handleRolesChange = (value: number[]) => {
+    setForm((prevForm) => ({
+      ...prevForm,
+      role_ids: value,
+    }));
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="flex min-h-screen w-full flex-col items-center gap-4 p-24 bg-neutral-100">
+   <div>
+      <DialogTrigger onOpen={open} />
+      <DialogOverlay isOpen={isOpen} onClose={close}>
+        <DialogContent side="bottom">
+          <DialogHeader>
+            <DialogTitle>Payment successful</DialogTitle>
+          </DialogHeader>
+          <p className="mt-2 text-sm/6 text-white/50">
+            Your payment has been successfully submitted. We’ve sent you an email with all of the details of your order.
+          </p>
+          <DialogFooter>
+            <Button
+               onClick={close}
+            >
+              Got it, thanks!
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </DialogOverlay>
+    </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div className="w-full max-w-sm">
+      <form action="#" className="flex flex-col space-y-6">
+       
+
+<Fieldset>
+        <Legend>Shipping details 256</Legend>
+        <Field>
+          <Label>Street address</Label>
+          <Input
+            placeholder="House number and street name"
+          />
+        </Field>
+
+      
+
+<Select
+        options={people}
+        value={form.person_id}
+        onChange={(value) => handlePersonChange(value)}
+      />
+
+        <MultipleSelect
+          placeholder="Select roles"
+          value={form.role_ids}
+          options={roles}
+          multiple
+          onChange={(value) => handleRolesChange(value)}
         />
-      </div>
+      </Fieldset>
+      </form>
+    </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
